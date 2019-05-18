4eee02ceae6315506aa1208c13ce9ea7c76e9ba4
pedestrianlove <j.s.li@gapp.nthu.edu.tw>
2019-05-19 07:34:25 +0800

    Fix the max-pooling problem

--- MID-2/MAX_POOL/function.c
+++ MID-2/MAX_POOL/function.c
@@ -7,7 +7,7 @@ void max_pooling (int A[500][500], int H, int W, int k)
 	for(int pt_row = 0; pt_row < (H/k); pt_row ++){
 		for (int pt_col = 0; pt_col < (W/k); pt_col++){
 			// For every submatrix, find max
-			max = A[pt_row][pt_col];
+			max = A[pt_row*k][pt_col*k];
 			for (int row = pt_row * k; row < (pt_row*k + k); row++){
 				for (int col = pt_col*k; col< (pt_col*k + k); col++){
 					printf("%d ", A[row][col]);
