84f43f60ca6f497af3ae4a8926f3ee935925cb11
pedestrianlove <j.s.li@gapp.nthu.edu.tw>
2019-06-14 18:19:13 +0800

    reverse chain TBD

---
+++ TEST/final_practice/rain/function.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+// global
+int list_length = 0;
+
+// operations
+Node* init_node (char*);
+Node* jump_node (Node** ,int);
+
+
+void insert (Node** head, char* color, int dst)
+{
+	Node* new = init_node (color);
+	Node* Dst = jump_node (head, list_length);
+	
+	new -> next = Dst -> next;
+	Dst -> next = new;
+	list_length ++;
+}
+
+void erase1 (Node** head, int dst)
+{
+	Node* Dst = jump_node (head, dst);
+	free (Dst);
+}
+
+void erase2(Node** head, char* dst)
+{
+	Node* ptr = *head;
+	Node* tmp;
+	while (ptr -> next != NULL) {
+		if (strcmp (ptr->next->color, dst) == 0) {
+			tmp = ptr -> next;
+			ptr -> next = ptr -> next -> next;
+			free (tmp);
+			continue;
+		}
+		ptr = ptr -> next;
+	}
+}
+
+void reverse(Node** head, int start, int dst) // FIXME
+{
+	if (start > dst)
+		reverse (Node** head, dst, start);
+
+	Node* Start = jump_node (head, start);
+	Node* Dst = jump_node (head, dst);
+	
+
+	
+}
+
+/*-------------------------------------------------------*/
+
+Node* init_node (char* color)
+{
+	Node* new = malloc (sizeof(node));
+	strcpy (new -> color, color);
+	new -> next = NULL;
+	return new;
+}
+
+Node* jump_node (Node** head, int dst)
+{
+	Node* ptr = *head;
+	if (list_length <= dst)
+		for (int i = 0; i < list_length; i++) {
+			ptr = ptr -> next;
+		}
+	else
+		for (int i = 0; i < dst; i++) {
+			ptr = ptr -> next;
+		}
+	return ptr;
+}
