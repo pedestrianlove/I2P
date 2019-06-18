0a5fd820ccda6d28060ade440fbf6820f869787b
pedestrianlove <j.s.li@gapp.nthu.edu.tw>
2019-06-18 22:01:45 +0800

    update buy

--- TEST/final_practice/buy/hw.c
+++ TEST/final_practice/buy/hw.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-#define MINE 1
-#define NOT_MINE 0
-
 typedef struct item {
-	int PRICE[2];
+	int PRICE[3];
 
-	int STATE;
 	int MIN_DAY;
 }item;
 
@@ -17,9 +13,7 @@ int LIST_LENGTH;
 
 // internal
 item* init_item ();
-int cmp_day1 (const void*, const void*);
-int cmp_day2 (const void*, const void*);
-int cmp_all (const void*, const void*);
+int cmp_diff (const void*, const void*);
 
 // interface
 void Init_list ();
@@ -33,8 +27,7 @@ int main()
 
 	Input_list ();
 
-	int sum = Analyze_list ();
-	printf("%d\n", sum);
+	printf("%d\n", Analyze_list ());
 	Delete_list ();
 	return 0;
 }
@@ -55,8 +48,7 @@ item* init_item ()
 	
 	new -> PRICE[0] = 0;
 	new -> PRICE[1] = 0;
-	new -> STATE = NOT_MINE;
-	new -> MIN_DAY = 3;
+	new -> MIN_DAY = 4;
 
 	return new;
 }
@@ -73,6 +65,9 @@ void Input_list ()
 		scanf("%d", &entry);
 		LIST[i] -> PRICE[1] = entry;
 	}
+	for (int i = 0; i < LIST_LENGTH; i++) {
+		LIST[i] -> PRICE[2]  =  LIST[i] -> PRICE[1]  -  LIST[i] -> PRICE[0];
+	}
 }
 
 
@@ -84,63 +79,30 @@ int Analyze_list (int first_day_quota) // FIXME Logic error
 	for (int i = 0; i < LIST_LENGTH; i++)
 		LIST[i]->MIN_DAY = (LIST[i]->PRICE[0] <= LIST[i]->PRICE[1])? 0 : 1;
 
-
-	qsort (LIST, LIST_LENGTH, sizeof(item*), cmp_all);	
-	int counter = 0;
-	for (int i = 0; i < LIST_LENGTH; i++) {
-		if (LIST[i] -> MIN_DAY == 0) {
-			counter++;
-			LIST[i] -> STATE = MINE;
-			min_sum += LIST[i]->PRICE[0];
-		}
-	}
-
-	qsort (LIST, LIST_LENGTH, sizeof(item*), cmp_day1);
-	for (int i = 0; i < LIST_LENGTH; i++) {
-		if (LIST[i]->STATE == NOT_MINE ) {
-			counter++;
-			LIST[i] -> STATE = MINE;
-			min_sum += LIST[i]->PRICE[0];
-		}
-		if (counter >= first_day_quota)
-			return // FIXME 
-	}
-
-
-	qsort (LIST, LIST_LENGTH, sizeof(item*), cmp_day2);
-	while (counter < LIST_LENGTH) {
-		int i = 0;
-		if (LIST[i]->STATE == NOT_MINE) {
-			counter++;
-			LIST[i] -> STATE = MINE;
-			min_sum += LIST[i] ->PRICE[1];	
-		}
-		i++;
-	}
+	for (int i = 0; i < LIST_LENGTH; i++)
+		printf("%d ", LIST[i]->PRICE[2]);
+	printf("\n");
+	qsort (LIST, LIST_LENGTH, sizeof(item*), cmp_diff);	
+	for (int i = 0; i < LIST_LENGTH; i++)
+		printf("%d ", LIST[i]->PRICE[2]);
+	printf("\n");
 	
+	for (int i = 0; i < LIST_LENGTH; i++) {
+		if (i < first_day_quota) {
+			min_sum += LIST[i] -> PRICE[0];
+		}
+		else
+			min_sum += LIST[i] -> PRICE[LIST[i]->MIN_DAY];
+	}
 
 	return min_sum;
 }
-int cmp_day1 (const void * a, const void * b)
+int cmp_diff (const void * a, const void * b) // decreasing
 {
 	const item * va = (const item *) a;
 	const item * vb = (const item *) b;
 
-	return va->PRICE[0] - vb->PRICE[0];
-}
-int cmp_day2 (const void * a, const void * b)
-{
-	const item * va = (const item *) a;
-	const item * vb = (const item *) b;
-
-	return va->PRICE[1] - vb->PRICE[1];
-}
-int cmp_all (const void * a, const void * b)
-{
-	const item * va = (const item *) a;
-	const item * vb = (const item *) b;
-
-	return va->PRICE[va->MIN_DAY] - vb->PRICE[va->MIN_DAY];
+	return va->PRICE[2] - vb->PRICE[2];
 }
 
 
