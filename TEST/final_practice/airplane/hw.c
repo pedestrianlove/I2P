2165f2a9677be9400c236b95de813db1de330d25
pedestrianlove <j.s.li@gapp.nthu.edu.tw>
2019-06-18 23:48:01 +0800

    added airplane

---
+++ TEST/final_practice/airplane/hw.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct data {
+	int INDEX;
+	int LEVEL;
+	int NUMBER;
+}data;
+data* init_data (int index, int level, int number)
+{
+	data* new = malloc (sizeof(data));
+	
+	new -> INDEX = index;
+	new -> LEVEL = level;
+	new -> NUMBER = number;
+	
+	return new;
+}
+
+/*-------------------------------------------------------------------*/
+// global
+data** LIST;
+int LIST_LENGTH;
+
+// internal
+
+
+// interface
+void Init_list ();
+void Get_list ();
+void Delete_list ();
+
+/*-------------------------------------------------------------------*/
+int main()
+{
+	Init_list ();
+
+	Get_list ();
+
+	Sort_list ();	
+
+	Delete_list ();
+
+	return 0;
+}
+
+
+void Init_list ()
+{
+	scanf("%d", &LIST_LENGTH);
+	LIST = malloc (sizeof(data*) * LIST_LENGTH);
+		
+	for (int i = 0; i < LIST_LENGTH; i++)
+		LIST[i] = NULL;	
+}
+
+
+void Get_list ()
+{
+	int level, number;
+	for (int i = 0; i < LIST_LENGTH; i++) {
+		scanf("%d%d", &level, &number);
+		LIST[i] = init_data (i+1, level, number);
+	}
+}
+
+
+void Sort_list ()
+{
+
+}
+
+
+void Delete_list ()
+{
+	for (int i = 0; i < LIST_LENGTH; i++)
+		free (LIST[i]);
+	free (LIST);
+}
